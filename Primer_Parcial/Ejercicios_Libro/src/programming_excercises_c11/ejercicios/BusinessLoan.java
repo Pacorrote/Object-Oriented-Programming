9095ad91ce4a4bc4fab5b5fc5e6f2c30f3c00b03
Francisco Árciga Nolasco <42156807+Pacorrote@users.noreply.github.com>
2019-02-28 21:28:49 -0600

    Update BusinessLoan.java

--- Primer_Parcial/Ejercicios_Libro/src/programming_excercises_c11/ejercicios/BusinessLoan.java
+++ Primer_Parcial/Ejercicios_Libro/src/programming_excercises_c11/ejercicios/BusinessLoan.java
@@ -1,15 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package programming_excercises_c11.ejercicios;
 
 
 public class BusinessLoan extends Loan{
     public BusinessLoan(Integer prestamo, String apellido, Integer monto, Integer plazo) {
         super(prestamo, apellido, monto, plazo);
-        this.tasaInteres=0.001f;
+        this.tasaInteres=(float) 0.001;
     }
     
 }
