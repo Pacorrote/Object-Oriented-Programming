2179bf1f7356c386317cdfd624ababd587953342
Francisco Árciga Nolasco <42156807+Pacorrote@users.noreply.github.com>
2019-02-28 21:21:18 -0600

    Update Book.java

--- Primer_Parcial/Ejercicios_Libro/src/programming_excercises_c11/ejercicios/Book.java
+++ Primer_Parcial/Ejercicios_Libro/src/programming_excercises_c11/ejercicios/Book.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package programming_excercises_c11.ejercicios;
 
 
@@ -14,13 +9,16 @@ public abstract class Book {
         this.titulo = titulo;
         this.precio = precio;
     }
-    public String getTitulo() {
-        return titulo;
-    }
 
-    public Double getPrecio() {
-        return precio;
-    }
-    public abstract void setPrice();
+    public String getTitulo() {
+		return titulo;
+	}
+
+	public Double getPrecio() {
+		return precio;
+	}
+
+
+    public abstract void setPrecio();
     
 }
