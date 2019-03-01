eb11ffd66fa4445552087170ab998717b31eec3f
Francisco Árciga Nolasco <42156807+Pacorrote@users.noreply.github.com>
2019-02-28 22:00:11 -0600

    Update IncomingPhoneCall.java

--- Primer_Parcial/Ejercicios_Libro/src/programming_excercises_c11/ejercicios/IncomingPhoneCall.java
+++ Primer_Parcial/Ejercicios_Libro/src/programming_excercises_c11/ejercicios/IncomingPhoneCall.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package programming_excercises_c11.ejercicios;
 
 
@@ -30,7 +25,7 @@ public class IncomingPhoneCall extends PhoneCall {
 
     @Override
     public void setPrecio() {
-        this.Precio=0.02f;
+        this.Precio=0.02;
     }
 
     
