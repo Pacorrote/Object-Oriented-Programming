2286c82c4d278233b4a79cc21a59c9e6fd423099
Francisco Árciga Nolasco <42156807+Pacorrote@users.noreply.github.com>
2019-02-28 21:51:35 -0600

    Update Employee.java

--- Primer_Parcial/Ejercicios_Libro/src/programming_excercises_c11/ejercicios/Employee.java
+++ Primer_Parcial/Ejercicios_Libro/src/programming_excercises_c11/ejercicios/Employee.java
@@ -1,17 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package programming_excercises_c11.ejercicios;
 
 
 public abstract class Employee {
-    public Integer id;
-    public String apellido;
-    public String nombre;
-    public Float pago;
-    public String titulo;
+    protected Integer id;
+    protected String apellido;
+    protected String nombre;
+    protected Float pago;
+    protected String titulo;
 
     public Integer getId() {
         return id;
