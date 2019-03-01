9217b00d7ed2d556c705091b6a5d597280600743
Francisco Árciga Nolasco <42156807+Pacorrote@users.noreply.github.com>
2019-02-28 22:05:09 -0600

    Update NoMetalElement.java

--- Primer_Parcial/Ejercicios_Libro/src/programming_excercises_c11/ejercicios/NoMetalElement.java
+++ Primer_Parcial/Ejercicios_Libro/src/programming_excercises_c11/ejercicios/NoMetalElement.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package programming_excercises_c11.ejercicios;
 
 
@@ -17,6 +12,6 @@ public class NoMetalElement extends Element{
             
     @Override
     public String describeElement() {
-       return String.format(this.name+"/"+this.symbol+"/"+" No. atomico: "+this.atomic+"/"+" Peso atomico: "+this.atomicWeigth);
-    }   
+       return String.format("%s/%s/"+" No. atomico: %d/ Peso atomico: ",this.name,this.symbol,this.atomic,this.atomicWeigth);
+    }    
 }
