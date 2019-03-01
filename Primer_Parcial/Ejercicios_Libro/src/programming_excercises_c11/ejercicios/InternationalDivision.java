cb1770d175a87876b8bdb8bbd5df5282cb9290a8
Francisco Árciga Nolasco <42156807+Pacorrote@users.noreply.github.com>
2019-02-28 21:48:36 -0600

    Update InternationalDivision.java

--- Primer_Parcial/Ejercicios_Libro/src/programming_excercises_c11/ejercicios/InternationalDivision.java
+++ Primer_Parcial/Ejercicios_Libro/src/programming_excercises_c11/ejercicios/InternationalDivision.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package programming_excercises_c11.ejercicios;
 
 public class InternationalDivision extends Division{
@@ -16,6 +11,6 @@ public class InternationalDivision extends Division{
 
     @Override
     public void display() {
-        System.out.println(" "+name+"/"+" No: "+account+"/"+" "+pais+"/"+" "+idioma);
+        System.out.println(" "+nombre+"/"+" No: "+cuenta+"/"+" "+pais+"/"+" "+idioma);
     }
 }
