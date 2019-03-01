6319d3ddfbbde90e0564f2056cf51b0ff4ba02bb
Francisco Árciga Nolasco <42156807+Pacorrote@users.noreply.github.com>
2019-02-28 21:58:41 -0600

    Update Life.java

--- Primer_Parcial/Ejercicios_Libro/src/programming_excercises_c11/ejercicios/Life.java
+++ Primer_Parcial/Ejercicios_Libro/src/programming_excercises_c11/ejercicios/Life.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package programming_excercises_c11.ejercicios;
 
 
@@ -20,7 +15,7 @@ public class Life extends Insurance {
     }
     @Override
     public void Setcos() {
-        this.Precio=36;
+        this.Precio=(double) 36;
     }
 
     @Override
