5d0e359c714e2a174c14f69a46e728132f2b2a35
Francisco Árciga Nolasco <42156807+Pacorrote@users.noreply.github.com>
2019-02-28 22:22:32 -0600

    Update Student.java

--- Primer_Parcial/Ejercicios_Libro/src/programming_excercises_c11/ejercicios/Student.java
+++ Primer_Parcial/Ejercicios_Libro/src/programming_excercises_c11/ejercicios/Student.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package programming_excercises_c11.ejercicios;
 
 public abstract class Student {
-    public String name;
-    public Boolean status;
+    protected String name;
+    protected Boolean status;
 
     public Student(String name,Boolean status) {
         this.name = name;
