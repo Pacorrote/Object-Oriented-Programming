88c23773513357368ba921bfb0d578688ef0871d
Francisco Árciga Nolasco <42156807+Pacorrote@users.noreply.github.com>
2019-02-28 21:27:35 -0600

    Update CardGame.java

--- Primer_Parcial/Ejercicios_Libro/src/programming_excercises_c11/ejercicios/CardGame.java
+++ Primer_Parcial/Ejercicios_Libro/src/programming_excercises_c11/ejercicios/CardGame.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package programming_excercises_c11.ejercicios;
 
 public abstract class CardGame {
-    public Integer repartidas;
-    public String mazo;
-    public Integer number;
+    protected Integer repartidas;
+    protected String mazo;
+    protected Integer number;
 
     public CardGame() {
         this.mazo="Corazones";
