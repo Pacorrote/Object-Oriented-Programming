156e28cafee160410283e7a1b2e4e265cf766e7a
Francisco Árciga Nolasco <42156807+Pacorrote@users.noreply.github.com>
2019-02-28 21:44:17 -0600

    Update DomesticDivision.java

--- Primer_Parcial/Ejercicios_Libro/src/programming_excercises_c11/ejercicios/DomesticDivision.java
+++ Primer_Parcial/Ejercicios_Libro/src/programming_excercises_c11/ejercicios/DomesticDivision.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package programming_excercises_c11.ejercicios;
 
 
@@ -15,7 +10,7 @@ public class DomesticDivision extends Division{
 
     @Override
     public void display() {
-        System.out.println(" "+name+"/"+" No: "+account+"/"+" "+state);
+        System.out.println(" "+nombre+"/"+" No: "+cuenta+"/"+" "+state);
     }
     
 }
