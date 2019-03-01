b29b21a3aa9c9baba79544a707081b55a9ada0ff
Francisco Árciga Nolasco <42156807+Pacorrote@users.noreply.github.com>
2019-02-28 22:10:32 -0600

    Update Novel.java

--- Primer_Parcial/Ejercicios_Libro/src/programming_excercises_c11/ejercicios/Novel.java
+++ Primer_Parcial/Ejercicios_Libro/src/programming_excercises_c11/ejercicios/Novel.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package programming_excercises_c11.ejercicios;
 
 public class Novel extends Story{
@@ -23,6 +18,10 @@ public class Novel extends Story{
     }
     @Override
     public String toString(){
-        return String.format(this.getTitle()+" del autor"+this.getAuthor()+" No."+this.getPages()+" "+ this.getMessage());
+        return String.format("%s del autor %s No.%d %s",this.getTitle(),
+        		this.getAuthor(),
+        		this.getPages(),
+        		this.getMessage(),this.getMessage());
     }
 }
+
