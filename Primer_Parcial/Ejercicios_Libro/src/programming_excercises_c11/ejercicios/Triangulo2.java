b3116eeb9ee4a070038c5f6e0879a456535498ff
Francisco Árciga Nolasco <42156807+Pacorrote@users.noreply.github.com>
2019-02-28 22:24:58 -0600

    Update Triangulo2.java

--- Primer_Parcial/Ejercicios_Libro/src/programming_excercises_c11/ejercicios/Triangulo2.java
+++ Primer_Parcial/Ejercicios_Libro/src/programming_excercises_c11/ejercicios/Triangulo2.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package programming_excercises_c11.ejercicios;
 
 public class Triangulo2 extends GeometricFigure2 implements SidedObject{
@@ -17,7 +12,7 @@ public class Triangulo2 extends GeometricFigure2 implements SidedObject{
     }
     @Override
     public String datos(){
-        return String.format(tipo+" de altura "+alto+" de ancho "+ancho+" tiene un area de: "+this.area()+" contiene una cantidad de lados de "+displaySides());
+        return String.format("%s de altura %d de ancho %d tiene un area de: %f contiene una cantidad de lados de %d",this.tipo,this.alto,this.ancho,displaySides());
     }
 
     @Override
